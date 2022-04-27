1be6c0abbfc6c05c6ba0a66cda5565854ea2bf6a
NewLaay <73190077+NewLaay@users.noreply.github.com>
2022-04-27 13:34:42 +0200

    Add files via upload

--- ExamenRecuFebrero/JSBasico/Ejercicio3/ejercicio3.js
+++ ExamenRecuFebrero/JSBasico/Ejercicio3/ejercicio3.js
@@ -1,35 +1,30 @@
 let palabra1 = "";
 let palabra2 = "";
 
-
 palabra1 = prompt("Escribe la primera palabra:");
-palabra2 = prompt("Escribe la segunda palabra y te diré si es anagrama de la 1: ");
-
-//Creamos array caracter a caracter y ordenamos.
-let arrayPal1 = palabra1.split("").sort();
-let arrayPal2 = palabra2.split("").sort();
+palabra2 = prompt("Escribe la segunda palabra y te dir� si es anagrama de la 1: ");
 
 function comprobarAnagramas(palabra1, palabra2){
-		//Si tienen diferente tamaño ya sabemos que son diferentes.
+		//Si tienen diferente tama�o ya sabemos que son diferentes.
     if(palabra1.length != palabra2.length){
     		return false;
     }
-    
+    //Ordenamos las palabras con el metodo sort.
+    palabra1.sort;
+    palabra2.sort;
     //Comprobar caracter a caracter que coinciden:
-    for(let i = 0; i<arrayPal1.length;i++){
-    	if(arrayPal1[i] != arrayPal2[i]){
+    for(let i = 0; i<palabra1.length;i++){
+    	if(palabra1[i] != palabra2[i]){
       	return false;
       }
       return true;
     }
 }
 
- if (comprobarAnagramas(palabra1,palabra2)){
-	console.log("Son anagramas. Comprobación: ");
-  console.log(arrayPal1);
-  console.log(arrayPal2);
+if (comprobarAnagramas(palabra1,palabra2)){
+	console.log("Son anagramas.");
 }else{
-	console.log("No son anagramas. Comprobación:");
-  console.log(arrayPal1);
-  console.log(arrayPal2);
-} 
+	console.log("No son anagramas.");
+}
+
+//Hay algun error... revisar.
