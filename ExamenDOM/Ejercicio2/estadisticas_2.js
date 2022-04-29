d3709e8d5ee642697662e1a3bd739e64c5bc2a8f
NewLaay <73190077+NewLaay@users.noreply.github.com>
2022-04-29 09:17:56 +0200

    Add files via upload

--- ExamenDOM/Ejercicio2/estadisticas_2.js
+++ ExamenDOM/Ejercicio2/estadisticas_2.js
@@ -1,7 +1,28 @@
-const conteoLetras = new Map();
+﻿const conteoLetras = new Map();
 
 function mostrarEstadisticas(textareaId){
     // INTRODUCIR CÓDIGO AQUÍ
+    //txtarea sera el valor que tenga el texto introducido por el usuario.
+    let txtarea = document.getElementById("textarea").value;
+    //Llamada a la funcion contarLetras en función del texto pasado y creación de numLetras.
+    let numLetras = contarLetras(txtarea);
+    //Creacion de los datos según las letras.
+    let datos = crearDatosEstadisticas(numLetras);
+    
+    //CREACION DE LA TABLA
+    let tBody = document.createElement("tbody");
+    let table = document.getElementsByTagName("table")[0];
+    table.appendChild(tBody);
+    
+    for (let i = 0; i<datos.length; i++){
+    	let tr = document.createElement("tr");
+      tBody.appendChild(tr);
+      for(let j = 0; j<datos[i].length;j++){
+      	let td = document.createElement("td");
+        td.innerHTML = datos[i][j];
+        tr.appendChild(td);
+      }
+    }
 }
 
 function contarLetras(texto) {
@@ -30,3 +51,5 @@ function crearDatosEstadisticas(numLetrasTotal){
 
     return arrayDatos;
 }
+
+
