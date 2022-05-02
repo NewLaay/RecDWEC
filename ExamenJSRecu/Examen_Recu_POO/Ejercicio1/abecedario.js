66b242ce20d8d409c6834138f8db086a2ff924aa
NewLaay <73190077+NewLaay@users.noreply.github.com>
2022-05-02 13:48:05 +0200

    Add files via upload

---
+++ ExamenJSRecu/Examen_Recu_POO/Ejercicio1/abecedario.js
@@ -0,0 +1,17 @@
+const arrayAbecedario = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const mapaAbecedarioUnicode = new Map();
+
+let contador = 0;
+for (let i=9398; i<=9423; i++) {
+    mapaAbecedarioUnicode.set(`${arrayAbecedario[contador]}`, `&#${i}`);
+    contador++;
+}
+
+
+
+
+
+
+
+
+
