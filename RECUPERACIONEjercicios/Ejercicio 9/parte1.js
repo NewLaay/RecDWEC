f45cb83d1440664cf8f452f51e8fcdc4c2206333
NewLaay <73190077+NewLaay@users.noreply.github.com>
2022-05-13 10:53:11 +0200

    Add files via upload
    finalizada parte 1

--- RECUPERACIONEjercicios/Ejercicio 9/parte1.js
+++ RECUPERACIONEjercicios/Ejercicio 9/parte1.js
@@ -15,19 +15,51 @@ for (let i = 0; i<preguntas.length;i++){
   p.id = `idPregunta${i}`; //Crear id para la pregunta
   let inputRespuesta = document.createElement("input"); //Cuadro de texto para poner la respuesta
   inputRespuesta.type = "text";
-  inputRespuesta.id = `idRespuesta${i}`; //Crear id para la respuesta
+  inputRespuesta.setAttribute("respuesta", i); //Crear id para la respuesta
   let validarRespuesta = document.createElement("input"); //Validar respuesta
   validarRespuesta.type = "button";
   validarRespuesta.value = "Aceptar";
-  validarRespuesta.onclick ="comprobarRespuesta(p.id, inputRespuesta.id)";
-  
+  validarRespuesta.style.marginLeft = "10px";
+  validarRespuesta.id = i;
+  //validarRespuesta.onclick ="comprobarRespuesta(p.id, inputRespuesta.id)";
+  	
  p.innerHTML = preguntas[i][0] + "    ";
   body.appendChild(p);
   p.appendChild(inputRespuesta);
+  let aSolucion = document.createElement("a");
+  aSolucion.style.margin = "2px";
+  aSolucion.setAttribute("solucion",i);
+  p.appendChild(aSolucion);
   p.appendChild(validarRespuesta);
   
 }
 
-function comprobarRespuesta(pregunta,respuesta){
-	
+//Añadimos un evento al hacer click: Si no es de tipo boton (si no clicamos en aceptar) no hacemos nada
+// Si es de tipo boton, llamamos a la funcion validarRespuesta con la id de la respuesta
+document.addEventListener('click',(e)=>{
+ if(e.target.type != 'button') return;
+ validarRespuesta(e.target.id);
+})
+
+//Funcion que validará respuesta en funcion del ID seleccionado.
+function validarRespuesta(resID){
+
+//Declaramos respuesta como el valor con QuerySelector que nos da el atributo del ID seleccionado
+//El metodo CSS.escape nos permite devolver el valor de ID 
+//Si recordamos, realizamos un SetAttribute para implantarlo (inputRespuesta.setAttribute("respuesta", i);)
+ let respuesta = document.querySelector('[respuesta=' + CSS.escape(resID) + "]").value;
+let mensajeSolucion = document.querySelector('[solucion=' + CSS.escape(resID) + "]");
+//Si la respuesta es correcta, comparamos con el arrayBidimensional en la 2 posicion, que son las respuestas. Creamos el mensaje que queremos.
+
+let mensajeP = "";
+let respuestaCorrecta = preguntas[resID][1];
+ if(respuesta == respuestaCorrecta){
+ 	mensajeP = "¡CORRECTO!";
+  mensajeSolucion.innerHTML = mensajeP;
+ 	return true;
+ }else {
+ 	mensajeP = "Lo sentimos, la respuesta correcta es " + respuestaCorrecta + ".";
+  mensajeSolucion.innerHTML = mensajeP;
+ 	return false; 
+ }
 }
